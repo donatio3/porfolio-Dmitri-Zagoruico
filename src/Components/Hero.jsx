7384ba5d7a2b318ccac050fa3ab283c0e57c6bb9
DIMA <sinatkron@gmail.com>
2024-07-29 16:55:00 +0300

    change to other name

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
                         variants={container(0)} 
                         initial="hidden"
                         animate="visible" 
-                        className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Dmitri Zagoruico</motion.h1>
+                        className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Anastasia Lazu</motion.h1>
                     <motion.span 
                         variants={container(0.5)} 
                         initial="hidden"
